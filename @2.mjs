a0e2f2765ded207d1ab801f3b73c6bc557b46f58
Kimberley Thill <ThillKimberleyPro@gmail.com>
2024-10-04 11:44:11 +0200

    Create @2.mjs

---
+++ @2.mjs
@@ -0,0 +1,14 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let getal1 = parseFloat(await userInput.question("Geef me uw eerste getal:  "));
+let getal2 = parseFloat(await userInput.question("Geef me uw tweede getal:  "));
+
+while (getal2 !== 0) {
+    let deler = getal1 % getal2;
+    getal1 = getal2;
+    getal2 = deler;
+
+} 
+console.log("De grootste gemene deler van " + getal1)
